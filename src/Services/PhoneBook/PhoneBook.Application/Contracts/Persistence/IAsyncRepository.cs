7e97ec617afd6b7e6818c1e3f2097498f71f3d0f
caner <canerdk1995@hotmail.com>
2023-05-26 00:41:48 +0300

    Created folders and IAsyncRepository in persistence.

---
+++ src/Services/PhoneBook/PhoneBook.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,25 @@
+﻿using PhoneBook.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : EntityBase
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        string includeString = null,
+                                        bool disableTracking = true);
+        Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null,
+                                       Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                       List<Expression<Func<T, object>>> includes = null,
+                                       bool disableTracking = true);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
