7191f1de56a31756852a441d97d36fd4c671af43
caner <canerdk1995@hotmail.com>
2023-05-26 00:30:42 +0300

    Created phonebook domain models.

---
+++ src/Services/PhoneBook/PhoneBook.Domain/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using PhoneBook.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Domain.Entities
+{
+    public class Person : EntityBase
+    {
+        public Person()
+        {
+            PersonContacts = new HashSet<PersonContact>();
+        }
+
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public virtual ICollection<PersonContact> PersonContacts { get; set; }
+    }
+}
