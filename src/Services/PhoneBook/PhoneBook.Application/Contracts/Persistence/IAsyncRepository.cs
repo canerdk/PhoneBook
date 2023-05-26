f05736ee4c11757d83a1327b11fef520b3846611
caner <canerdk1995@hotmail.com>
2023-05-26 17:03:54 +0300

    Added dbcontext and repository classes.

--- src/Services/PhoneBook/PhoneBook.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/PhoneBook/PhoneBook.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -1,21 +1,13 @@
-﻿using PhoneBook.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using PhoneBook.Domain.Common;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneBook.Application.Contracts.Persistence
 {
     public interface IAsyncRepository<T> where T : EntityBase, new()
     {
-        Task<T> GetAsync(Expression<Func<T, bool>> predicate,
-                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                        string includeString = null);
-        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null,
-                                       Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                       List<Expression<Func<T, object>>> includes = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<T> DeleteAsync(T entity);
---
+++ src/Services/PhoneBook/PhoneBook.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneBook.Application.Contracts.Persistence;
+using PhoneBook.Infrastructure.Persistence;
+using PhoneBook.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("PhoneBookConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+
+
+
+            return services;
+        }
+    }
+}
