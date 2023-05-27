a3c3cae6e9b0edb1b0df0b669493bd177b197c7a
caner <canerdk1995@hotmail.com>
2023-05-27 23:34:47 +0300

    Configured docker for report api

---
+++ src/Services/Report/Report.API/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Report.API.Repositories.Abstract;
+
+namespace Report.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IPhoneBookReportRepository _repository;
+
+        public ReportsController(IPhoneBookReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _repository.GetAll();
+            return Ok(result);
+        }
+    }
+}
